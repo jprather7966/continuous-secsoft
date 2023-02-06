dfa9bcf0d6d02bd6fba7a8e8fe95f483a6050a20
Jared Prather <39931445+jprather7966@users.noreply.github.com>
2023-02-06 09:24:24 -0600

    Workshop 1 Complete
    Finished Workshop 1

--- software-quality-assurance/workshops/workshop1/source.py
+++ software-quality-assurance/workshops/workshop1/source.py
@@ -1,2 +1,10 @@
 def performSub(x, y):
     return x - y 
+
+def performMult(x,y):
+    return x * y
+
+def performDiv(x,y):
+    if(y == 0):
+        return ZeroDivisionError
+    return x / y
--- software-quality-assurance/workshops/workshop1/test.py
+++ software-quality-assurance/workshops/workshop1/test.py
@@ -4,10 +4,25 @@ import source
 
 class TestCalc(unittest.TestCase):
     def testSub1(self):
-        self.assertEqual(1, source.performSub(2, 1), "Bug in implementation. Results should be 1.")    
+        self.assertEqual(1, source.performSub(2, 1), "Results should be 1.")    
 
     def testSub2(self):
-        self.assertEqual(10, source.performSub(20, 10), "Bug in implementation. Results should be 1.")    
+        self.assertEqual(31, source.performSub(53, 22), "Results should be 31.")  
+
+    def testMult1(self):
+        self.assertEqual(286, source.performMult(22, 13), "Results should be 286.")
+    
+    def testMult2(self):
+        self.assertEqual(50, source.performMult(5, 10), "Results should be 50.")
+
+    def testDiv1(self):
+        self.assertEqual(2, source.performDiv(20, 10), "Results should be 2.")
+
+    def testDiv2(self):
+        self.assertEqual(ZeroDivisionError, source.performDiv(20, 0), "Divide by Zero test case. Results should throw a ZeroDivisonError.")
+    
+    def testDiv3(self):
+        self.assertEqual(0, source.performDiv(0, 20), "Results should be 0.")
 
 if __name__ == '__main__': 
     unittest.main() 
